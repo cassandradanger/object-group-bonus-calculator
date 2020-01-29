fd8d9388985bc63a2a9e0412bce88d5235773514
Casie Siekman <casie.siekman@gmail.com>
2020-01-29 11:08:15 -0600

    initial commit

--- client.js
+++ client.js
@@ -41,3 +41,65 @@ const employees = [
 // Ask questions when you don't.
 
 console.log( employees );
+
+// ## Processing Employee Bonuses
+
+// Loop over the `employees` array and do the following:
+// * use each employee object as the input to the function described below.
+// * `console.log` the results of each iteration.
+
+for (let i= 0; i < employees.length; i++){
+  bonusCalculator(employees[i]);
+}
+// ## Function Logic
+
+// Write a declared function that takes in one **Employee** object (as an argument to the function), and returns a new **object** with the following properties:
+
+function bonusCalculator(employee){
+  // console.log('this is employee', employee);
+  let newObject = {
+    name: employee.name,
+    bonusPercentage: 0,
+    totalCompensation: 0,
+    totalBonus: 0
+  }
+
+  // this block of code assigns bonus percentage to the new object
+  if (employee.reviewRating <= 2){
+    newObject.bonusPercentage = 0;
+  } else if (employee.reviewRating == 3){
+    newObject.bonusPercentage = .04;
+  } else if (employee.reviewRating == 4){
+    newObject.bonusPercentage = .06;
+  } else if (employee.reviewRating == 5){
+    newObject.bonusPercentage = .10;
+  }
+
+  if (employee.employeeNumber.length == 4 && employee.reviewRating > 2){
+    newObject.bonusPercentage += .05;
+  }
+  
+  if(employee.annualSalary > 65000){
+    // bring bonusPercentage down .01;
+    newObject.bonusPercentage -= .01;
+  }
+
+  if (newObject.bonusPercentage > .13){
+    newObject.bonusPercentage = .13;
+  } else if (newObject.bonusPercentage < 0){
+    newObject.bonusPercentage = 0;
+  }
+
+  newObject.totalBonus = Number(employee.annualSalary) * newObject.bonusPercentage;
+  newObject.totalCompensation = Number(employee.annualSalary) + newObject.totalBonus;
+
+  console.log('final object', newObject);
+}
+
+// * The `totalCompensation` property should be the adjusted annual compensation (base annual + bonus)
+// * The `totalBonus` should be the employee's total bonus rounded to the nearest dollar.
+
+// ### Individual Bonus calculation
+// - No bonus can be above 13% or below 0% total.
+
+// NOTE: You may abstract out this bonus calculation into a second function if you like, but this is not mandatory.
